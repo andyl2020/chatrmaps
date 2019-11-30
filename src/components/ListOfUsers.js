c3a267d067a5c2e21a63f39bf07c0451711b1a72
derek c <derekc150@gmail.com>
2019-11-30 14:27:32 -0800

    finished ListOfUsers using materials-ui

--- src/components/ListOfUsers.js
+++ src/components/ListOfUsers.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem'
+import { ListItemText } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid'
 
 export class ListOfUsers extends Component {
     state = {
@@ -8,13 +11,33 @@ export class ListOfUsers extends Component {
             { name: 'josh' },
             { name: 'andy' },
             { name: 'clarence' }
+            { name: 'matthrew' }
         ]
     }
+    users = this.state.users.map((user) => (
+        <Grid container>
+            <Grid item xs={11}>
+                <ListItem>
+                    <ListItemText>{user.name}</ListItemText>
+                </ListItem>
+            </Grid>
+            <Grid item xs={1}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z" />
+                </svg>
+            </Grid>
+        </Grid>
+    ))
+
     render() {
-        return this.state.users.map((user) => (
-            <Typography>{user.name}</Typography>
-        ));
+        return (
+            <div>
+                <List >
+                    {this.users}
+                </List>
+            </div>
+        );
     }
 }
 
-export default ListOfUsers
+export default ListOfUsers;
