f42366667f8a589a3c2b494d3f84c30e1599e43b
Josh Marangoni <joshua.marangoni@gmail.com>
2019-11-30 14:08:38 -0800

    click on event

---
+++ src/components/Event.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import React from "react";
+
+class Event extends Component {
+  state = {
+    text: ""
+  }
+
+  onChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    if(this.state.text) {
+      this.setState({ text: "" });
+      this.props.onSendMessage(this.state.text);
+    }
+  }
+
+  render() {
+    return (
+      <div className = "Event"
+        style = {this.props.show ? {display:"block"} : {display:"none"}}>
+        <form onSubmit = { e => this.onSubmit(e) }>
+          <input
+            onChange    = { e => this.onChange(e) }
+            value       = { this.state.text }
+            type        = "text"
+            placeholder = "Enter your message and press ENTER"
+          />
+          <button>Send</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Event;
