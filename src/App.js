7d26815f1b92709d2709a5d3bfa73120f320ae31
ad2969 <Clarenceadrian@gmail.com>
2019-11-29 23:06:09 -0800

    clode cleaining, implement unique message keys

--- src/App.js
+++ src/App.js
@@ -1,39 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
-import Messages from "./Messages";
-import Input from "./Input";
-
-function randomName() {
-  const adjectives = [
-    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
-    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
-    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
-    "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long",
-    "late", "lingering", "bold", "little", "morning", "muddy", "old", "red",
-    "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering",
-    "withered", "wild", "black", "young", "holy", "solitary", "fragrant",
-    "aged", "snowy", "proud", "floral", "restless", "divine", "polished",
-    "ancient", "purple", "lively", "nameless"
-  ];
-  const nouns = [
-    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
-    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
-    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
-    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
-    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
-    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
-    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
-    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog",
-    "smoke", "star"
-  ];
-  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  return adjective + noun;
-}
-
-function randomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
+import './styles/App.css';
+import Messages from "./components/Messages";
+import Input from "./components/Input";
+import { randomName, randomColor } from "./functions/demo";
 
 class App extends Component {
   state = {
@@ -53,30 +22,30 @@ class App extends Component {
       if (error) {
         return console.error(error);
       }
-      const member = {...this.state.member};
+      const member = { ...this.state.member };
       member.id = this.drone.clientId;
-      this.setState({member});
+      this.setState({ member });
     });
     const room = this.drone.subscribe("observable-room");
     room.on('data', (data, member) => {
       const messages = this.state.messages;
-      messages.push({member, text: data});
-      this.setState({messages});
+      messages.push({ member, text: data });
+      this.setState({ messages });
     });
   }
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>My Chat App</h1>
+      <div className = "App">
+        <div className = "App-header">
+          <h1>Chatr 2.0</h1>
         </div>
         <Messages
-          messages={this.state.messages}
-          currentMember={this.state.member}
+          messages      = { this.state.messages }
+          currentMember = { this.state.member }
         />
         <Input
-          onSendMessage={this.onSendMessage}
+          onSendMessage = { this.onSendMessage }
         />
       </div>
     );
