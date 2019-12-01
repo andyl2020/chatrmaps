fc72e66ec43d81fd4f44c92dc8ec893dc2f72b45
ad2969 <clarenceadrian@alumni.ubc.ca>
2019-11-30 19:37:35 -0800

    nuke multiple people

--- src/components/small/marker.js
+++ src/components/small/marker.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import { Room, Person, People } from '@material-ui/icons';
+import { Room } from '@material-ui/icons';
 
 class Marker extends Component {
   render() {
     var icon = <Room className = "marker"/>;
-    if(this.props.num > 0) {
-      icon = this.props.num > 1 ? <People className = "marker"/> : <Person className = "marker"/>;
-    }
     return(
       <div  className="markerHolder"
             onClick = {() => {this.props.clickEvent(this.props.lat, this.props.lng); }} >
